7202d20a090c95346dac2b3ae60345ebd46f462f
Yuel Abraham <yuel.abraham1@gmail.com>
2025-04-24 19:25:16 -0700

    Completed 01 - 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(faren) {
+  return Math.round(((faren - 32) * 5/9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  return Math.round(((cels * 9/5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
